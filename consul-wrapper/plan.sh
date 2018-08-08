081bdc14ea50b907f9b7ceeb561dbbbdd96d085f
Ian Henry <ihenry@chef.io>
2018-08-08 10:29:41 -0400

    adds dummy wrapper for testing
    Signed-off-by: Ian Henry <ihenry@chef.io>

---
+++ consul-wrapper/plan.sh
@@ -0,0 +1,15 @@
+pkg_name=consul-wrapper
+pkg_origin=eeyun
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkeg_license=("Apache-2.0")
+pkg_deps=(eeyun/consul)
+pkg_description="Some shit"
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    return 0
+}
